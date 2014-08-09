4f67c247f8129918b204c9ac7ec11de23c594045
unknown <pnchappy92@gmail.com>
2014-08-09 02:57:31 -0700

    created a user

--- app.js
+++ app.js
@@ -8,19 +8,39 @@ var app = express();
 app.use(bodyParser());
 app.use(express.static(__dirname + "/public"));
 
-function collection(collection) {  
+function collection(collection, callback) {  
 	MongoClient.connect('mongodb://127.0.0.1:27017/formbuilder', function(err, db) {
-		if(err) throw err;
-		callback(db.collection(collection));
+		callback(err, db.collection(collection));
 	});
 }
 
 app.post("/user/login", function(req, res) {
 	request("https://graph.facebook.com/v2.1/me?access_token=" + req.body.accessToken, function(error, response, body) {
-		console.log(body);
+		if(error) {
+			res.send({
+				error : error
+			});
+		} else {
+			var accessToken = Guid.raw();
+			var expire = new Date().getTime() + (1000 * 60 * 60 * 1);
+			collection("user", function(err, user) {
+				user.findAndModify({facebook:response.id}, [], {accessToken:accessToken,expire:expire}, {}, function(err, u) {
+					if(!u) {
+						user.insert({
+							accessToken : accessToken,
+							expire : expire,
+							facebook : response.id,
+							inserted : new Date()
+						}, function(err){});
+					}
+					res.send({
+						data : accessToken
+					});
+				});
+			});
+		}
 	});
-	var accessToken = Guid.raw();
-	res.send(accessToken)
+	
 });
 
 app.listen(3000, function() {
