a6cdd933522404ce6a885308cb2c54a263f11c3e
Logan <logan.murphy@temple.edu>
2014-08-09 02:22:24 -0400

    added some canvas code and mongo collection wrapper

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var MongoClient = require('mongodb').MongoClient;
 var http = require("http");
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -5,6 +6,13 @@ var request = require("request");
 var app = express();
 app.use(bodyParser());
 app.use(express.static(__dirname + "/public"));
+
+function collection(collection) {  
+	MongoClient.connect('mongodb://127.0.0.1:27017/formbuilder', function(err, db) {
+		if(err) throw err;
+		callback(db.collection(collection));
+	});
+}
 
 app.post("/user/login", function(req, res) {
 	request("https://graph.facebook.com/v2.1/me?access_token=" + req.body.accessToken, function(error, response, body) {
--- public/templates/signature/controller.js
+++ public/templates/signature/controller.js
@@ -1,5 +1,14 @@
 formbuilder.controller("signature", ["$scope", function($scope) {
 	$scope.init = function(component) {
+		$scope.canvas = angular.element(document.querySelector("#" + $scope.component.properties.Name.value));
+		$scope.context = $scope.getContext("2d");
 		$scope.common();
+	};
+	
+	$scope.sign = function($event) {
+		
+	};
+	$scope.stop = function($event) {
+		
 	};
 }]);
